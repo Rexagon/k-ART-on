c5b874793c3187d7aab2e49212974190e561ae86
Ivan <reide740@gmail.com>
2016-01-30 18:53:13 +0300

    Some changes in server

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Configure app
+var app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "views"));
+
+// Use middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'views')));
+
+// Define routes
+app.use('/', require('./routes/index'));
+
+app.listen(1337, function() {
+  console.log('Server is running on port 1337');
+});
